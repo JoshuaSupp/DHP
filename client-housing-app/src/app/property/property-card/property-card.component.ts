c9bf63acf4740215c4c18b243e3de4d8b9a4a761
JoshuaSupp <89426183+JoshuaSupp@users.noreply.github.com>
2025-05-27 18:53:14 +0530

    Initial commit

---
+++ client-housing-app/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-property-card',
+    templateUrl: 'property-card.component.html',
+    styleUrls: ['property-card.component.css'],
+    standalone: true
+})
+
+
+export class PropertyCardComponent{
+
+    Property: any={
+        "id": 1,
+        "Name": "Dubai House",
+        "Type": "House",
+        "Price": 12000
+    }
+
+}
